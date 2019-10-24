3061ef8a8dc7277d5b5bc38070b4e47ed5834a49
Leonardo Hdez <perros_rebeldes@hotmail.com>
2019-10-24 16:54:00 -0500

    Acomodando por metodos

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 public class Main {
     static Scanner sc = new Scanner(System.in);
     static int numPlayers;
-    static String namePlayer[] = new String[numPlayers];
-    static int randomNumbers[] = new int[numPlayers];
-    static int maxNumber = randomNumbers[0];
-    static String persona = namePlayer[0];
+
 
     public static void main(String[] args) throws IOException {
+        startGame();
+    }
+
+    public static void startGame() throws IOException{
         System.out.println("Cuantos jugadores van a jugar?");
         numPlayers = sc.nextInt();
 
+        String namePlayer[] = new String[numPlayers];
         for(int i=0;i<numPlayers;i++){
             System.out.println("Introduce el nombre del jugador "+(i+1));
             namePlayer[i] = sc.next();
@@ -21,14 +23,18 @@ public class Main {
         for (int j=0; j<numPlayers; j++){
             System.out.println(namePlayer[j]);
         }
+        int randomNumbers[] = new int[numPlayers];
 
         for (int k=0; k<numPlayers; k++){
             System.out.print("\nLanza el dado "+namePlayer[k]+ "(Presiona Enter)");
             System.in.read();
+
+
             randomNumbers[k] = (int)(Math.random()*5+1);
             System.out.println("El resultado es: "+ randomNumbers[k]);
         }
-
+        int maxNumber = randomNumbers[0];
+        String persona = namePlayer[0];
         for(int a=0;a<numPlayers;a++){
             if (maxNumber<randomNumbers[a]){
                 System.out.println("Unos valores se repiten");
@@ -37,11 +43,6 @@ public class Main {
             }
         }
         System.out.println("Numero maximo "+maxNumber+" persona: "+namePlayer.toString());
-        
-    }
-
-    public static void startGame(){
-
     }
 
 }
