57b7a011ac88516aa66bc61bde6ba88987f0aebc
Leonardo Hdez <perros_rebeldes@hotmail.com>
2019-10-24 16:11:34 -0500

    Inicio del juego, pregunta nombres

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,39 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int numPlayers;
+        player pl1;
+
+
+
+
+        System.out.println("Cuantos jugadores van a jugar?");
+        numPlayers = sc.nextInt();
+        String namePlayer[] = new String[numPlayers];
+        for(int i=0;i<numPlayers;i++){
+            System.out.println("Introduce el nombre del jugador "+(i+1));
+            namePlayer[i] = sc.next();
+        }
+        System.out.println("Los jugadores son: ");
+        for (int j=0; j<numPlayers; j++){
+            System.out.println(namePlayer[j]);
+        }
+        for (int k=0; k<numPlayers; k++){
+            System.out.print("\nLanza el dado "+namePlayer[k]+ "(Presiona Enter)");
+            System.in.read();
+            System.out.println("El resultado es: "+((int)(Math.random()*5)+1));
+        }
+        System.out.println();
 
     }
+
+    public void startGame(){
+
+    }
+
 }
--- src/player.java
+++ src/player.java
@@ -1,3 +1,18 @@
 public class player {
     String nombre;
+
+    public player() {
+    }
+
+    public player(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
