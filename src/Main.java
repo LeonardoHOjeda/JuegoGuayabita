c8e73f86c0d4753f6de02cb4f600db49d4e06039
Leonardo Hdez <perros_rebeldes@hotmail.com>
2019-10-28 14:27:20 -0600

    Juego casi terminado

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 public class Main {
     static Scanner sc = new Scanner(System.in);
     static int numPlayers;
-    static int apuesta;
+    static int apuesta = 0;
     static int dice;
+    static int respuestaDice;
 
 
     public static void main(String[] args) throws IOException {
@@ -33,40 +34,20 @@ public class Main {
         /**
          * Listado de los jugadores disponibles
          */
-        System.out.println("Los jugadores son: ");
-        for (int j=0; j<numPlayers; j++){
-            System.out.println("Jugador "+(j+1)+": "+arrayPlayers[j].getNombre()+" Dinero: $"+arrayPlayers[j].getDinero());
-            //System.out.println("Jugador "+(j+1)+": "+namePlayer[j]);
-        }
+        showPlayers(arrayPlayers);
 
+        /**
+         * Se empieza a apostar
+         */
+        apuestaGeneral(arrayPlayers);
 
-        int randomNumbers[] = new int[numPlayers]; //Numeros aleatorios
-
-        for (int k=0; k<numPlayers; k++){// Lanzamiento de dado
-            System.out.println("Lanza el dado "+arrayPlayers[k].getNombre()+ "(Presiona Enter)");
-            System.in.read();
-            dice = (int)(Math.random()*(5+1)+1);
-
-            System.out.println("Primer lanzamiento: "+dice);
-                System.out.println("Vuelve a lanzar el dado");
-                System.in.read();
-                randomNumbers[k] = (int)(Math.random()*(5+1)+1);
-
-                if(randomNumbers[k] > dice){
-                    System.out.println("Ganaste! Recoge una moneda");
-                    arrayPlayers[k].setDinero(arrayPlayers[k].getDinero()+1);
-                    System.out.println("El dinero de "+arrayPlayers[k].getNombre()+" es de: "+arrayPlayers[k].getDinero());
-                } else {
-                    System.out.println("Perdiste, entrega una moneda");
-                    arrayPlayers[k].setDinero(arrayPlayers[k].getDinero()-1);
-                    System.out.println("El dinero de "+arrayPlayers[k].getNombre()+" es de: "+arrayPlayers[k].getDinero());
-                }
-                System.out.println("El resultado es: "+ randomNumbers[k]+"\n");
-        }
-
+        /**
+         * Empieza el juego lanzando el dado
+         */
         do {
-
+            lanzarDado(arrayPlayers);
         }while (apuesta > 0);
+        System.out.println("El juego ha terminado!");
 
         int maxNumber = 0;//randomNumbers[0];
         String persona = namePlayer[0];
@@ -90,7 +71,94 @@ public class Main {
         }*/
 
 
-        System.out.println("\nEmpieza a tirar el jugador: "+persona+" Numero maximo "+maxNumber+" persona: "+persona);
+        //System.out.println("\nEmpieza a tirar el jugador: "+persona+" Numero maximo "+maxNumber+" persona: "+persona);
+    }
+
+    public static void apuestaGeneral(player arrayPlayer[]){
+        System.out.println("\nTODOS LOS JUGADORES APUESTAN!");
+        for (int i=0; i<arrayPlayer.length; i++){
+            System.out.print(arrayPlayer[i].getNombre() + " ya puso su dinero!");
+            arrayPlayer[i].setDinero(arrayPlayer[i].getDinero()-1);
+            System.out.println("\tDinero actual: "+arrayPlayer[i].getDinero());
+            apuesta++;
+        }
+    }
+
+    public static void lanzarDado(player arrayPlayers[]) throws IOException {
+        int randomNumbers[] = new int[numPlayers]; //Numeros aleatorios
+        int pivote = 0;
+
+        for (int k=0; k<numPlayers; k++){// Lanzamiento de dado
+            System.out.print("\nLanza el dado "+arrayPlayers[k].getNombre()+ "(Presiona Enter) **APUESTA $"+apuesta+"** **DINERO $"+arrayPlayers[k].getDinero()+"**");
+            System.in.read();
+            dice = (int)(Math.random()*(5+1)+1);
+
+            System.out.println("Primer lanzamiento: "+dice);
+            System.out.println("Que quieres hacer?");
+            System.out.println("1) Ir por la apuesta!");
+            System.out.println("2) Ir por TODOOO!");
+            respuestaDice = sc.nextInt();
+            switch (respuestaDice){
+                case 1:
+                    if(arrayPlayers[k].getDinero() >= 1){
+                        System.out.println("Elegiste apuesta!");
+                        System.out.print("Vuelve a lanzar el dado!");
+                        System.in.read();
+                        randomNumbers[k] = (int)(Math.random()*(5+1)+1);
+                        System.out.println("Segundo lanzamiento: "+ randomNumbers[k]+"\n");
+                        if(randomNumbers[k] > dice){
+                            System.out.println("Ganaste! Recoge una moneda");
+                            arrayPlayers[k].setDinero(arrayPlayers[k].getDinero()+1);
+                            apuesta--;
+                            System.out.println("El dinero de "+arrayPlayers[k].getNombre()+" es de: "+arrayPlayers[k].getDinero());
+                        } else {
+                            System.out.println("Perdiste, entrega una moneda");
+                            arrayPlayers[k].setDinero(arrayPlayers[k].getDinero()-1);
+                            apuesta++;
+                            System.out.println("El dinero de "+arrayPlayers[k].getNombre()+" es de: "+arrayPlayers[k].getDinero());
+                        }
+                    } else{
+                        System.out.println("Lo sentimos, no tienes fondos suficientes para realizar esta accion");
+                        break;
+                    }
+                    break;
+                case 2:
+                    if (arrayPlayers[k].getDinero() >= apuesta){
+                        System.out.println("*********ELEGISTE TODO!!********");
+                        System.out.print("Vuelve a lanzar el dado!");
+                        System.in.read();
+                        randomNumbers[k] = (int)(Math.random()*(5+1)+1);
+                        System.out.println("Segundo lanzamiento: "+ randomNumbers[k]+"\n");
+                        if(randomNumbers[k] > dice){
+                            System.out.println("Ganaste! Recoge todas las monedas!");
+                            arrayPlayers[k].setDinero(arrayPlayers[k].getDinero() + apuesta);
+                            apuesta -= apuesta;
+                            System.out.println("El dinero de "+arrayPlayers[k].getNombre()+" es de: "+arrayPlayers[k].getDinero());
+                        } else {
+                            System.out.println("Perdiste, entrega "+apuesta+" monedas");
+                            pivote = arrayPlayers[k].getDinero();
+                            arrayPlayers[k].setDinero(arrayPlayers[k].getDinero()-apuesta);
+                            apuesta += apuesta;
+                            System.out.println("El dinero de "+arrayPlayers[k].getNombre()+" es de: "+arrayPlayers[k].getDinero());
+                        }
+                    } else {
+                        System.out.println("Lo sentimos, no tienes fondos suficientes para realizar esta accion");
+                        break;
+                    }
+                    break;
+                default:
+                    System.out.println("Esa opcion no existe");
+            }
+            System.out.println("Dinero actual de la apuesta: $"+apuesta);
+
+        }
+    }
+
+    public static void showPlayers(player arrayPlayers[]){
+        System.out.println("Los jugadores son: ");
+        for (int j=0; j<numPlayers; j++){
+            System.out.println("Jugador "+(j+1)+": "+arrayPlayers[j].getNombre()+" Dinero: $"+arrayPlayers[j].getDinero());
+        }
     }
 }
 
